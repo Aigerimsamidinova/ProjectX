27e4328ba706bee4d51a43612270fd5539b44d9c
admin <aesenbekova8>
2019-05-07 00:13:26 +1000

    added clientRep and clientController

--- src/main/java/com/example/Project/model/Client.java
+++ src/main/java/com/example/Project/model/Client.java
@@ -1,4 +1,65 @@
 package com.example.Project.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fio;
+
+    private String number;
+
+    @OneToMany
+    @JoinColumn(name = "item_id")
+    private List<Item> items;
+
+    public Client() {
+    }
+
+    public Client(String fio, String number, List<Item> items) {
+        this.fio = fio;
+        this.number = number;
+        this.items = items;
+    }
+
+    public Client(String fio, String number) {
+        this.fio = fio;
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
