9ecbbfce15ee44af714aa7ff4c6c5f7d7647a8eb
Kanat Subanov <subanovkanat321@gmail.com>
2019-05-07 00:05:38 +0600

    added item controller without mistakes

--- src/main/java/com/example/Project/controller/ItemController.java
+++ src/main/java/com/example/Project/controller/ItemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/office")
+@RequestMapping("/item")
 public class ItemController {
     @Autowired
     private CrudService<Item> itemCrudService;
