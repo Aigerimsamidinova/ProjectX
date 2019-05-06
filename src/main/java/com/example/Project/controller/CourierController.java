2d6c8c1f4f3eb782fb825ffcff944aa5548ef7d1
Kanat Subanov <subanovkanat321@gmail.com>
2019-05-07 00:04:31 +0600

    added item controller

--- src/main/java/com/example/Project/controller/CourierController.java
+++ src/main/java/com/example/Project/controller/CourierController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/office")
+@RequestMapping("/courier")
 public class CourierController {
     @Autowired
     private CrudService<Courier> courierCrudService;
--- src/main/java/com/example/Project/controller/ItemController.java
+++ src/main/java/com/example/Project/controller/ItemController.java
@@ -1,4 +1,69 @@
 package com.example.Project.controller;
 
+import com.example.Project.model.Item;
+import com.example.Project.service.CrudService;
+import com.example.Project.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/office")
 public class ItemController {
+    @Autowired
+    private CrudService<Item> itemCrudService;
+
+    @GetMapping(path = "/getAll", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<Item> getAll() {
+        return itemCrudService.getAll();
+    }
+
+    @GetMapping(path = "/get/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Response getById(@PathVariable Long id) {
+        try {
+            return new Response("Item by id = " + id, true, itemCrudService.findById(id));
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @PostMapping(path = "/add", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Response add(@RequestBody Item item) {
+        try {
+            return new Response("Successfully created", true, itemCrudService.save(item));
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @PutMapping(path = "/update", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    private Response updateOffice(Item item) {
+        try {
+            return new Response("Updated", true, itemCrudService.update(item));
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @DeleteMapping(path = "/delete/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Response deleteById(@PathVariable Long id) {
+        try {
+            itemCrudService.deleteById(id);
+            return new Response("Deleted item by id = " + id, true, null);
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @DeleteMapping(path = "/deleteAll", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    private Response deleteAll() {
+        try {
+            itemCrudService.deleteAll();
+            return new Response("Deleted all items", true, null);
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
 }
