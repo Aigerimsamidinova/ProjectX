ce1680466ca83b37131b660868b3f685ef7b7053
Kanat Subanov <subanovkanat321@gmail.com>
2019-05-06 23:33:01 +0600

    added bootstrap

--- src/main/java/com/example/Project/bootstrap/Main.java
+++ src/main/java/com/example/Project/bootstrap/Main.java
@@ -1,4 +1,36 @@
 package com.example.Project.bootstrap;
 
-public class Main {
+import com.example.Project.enums.Pointer;
+import com.example.Project.model.Application;
+import com.example.Project.model.Office;
+import com.example.Project.repository.AppRep;
+import com.example.Project.service.CrudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Main implements CommandLineRunner {
+    @Autowired
+    private AppRep appRep;
+
+    @Autowired
+    private CrudService<Office> officeCrudService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Application application = new Application.Builder("222","Gogolya 112-8", Pointer.BISHKEK)
+                .withEndPoint(Pointer.OSH)
+                .withFio("Kanat Subanov")
+                .withnameItem("Konvert")
+                .withWeightItem(0.01)
+                .build();
+        appRep.save(application);
+
+        Office office1 = new Office.Builder(Pointer.BISHKEK).build();
+        officeCrudService.save(office1);
+
+        Office office2 = new Office.Builder(Pointer.OSH).build();
+        officeCrudService.save(office2);
+    }
 }
