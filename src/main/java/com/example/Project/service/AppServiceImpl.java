9721c7ad6affcded3ef33a0ee78c81bb359e8f78
Lenovo YOGA <aigerimsamidinovaa@gmail.com>
2019-05-06 23:01:05 +0600

    Application service is done

--- src/main/java/com/example/Project/service/AppServiceImpl.java
+++ src/main/java/com/example/Project/service/AppServiceImpl.java
@@ -1,4 +1,43 @@
 package com.example.Project.service;
 
-public class AppServiceImpl {
+import com.example.Project.model.Application;
+import com.example.Project.repository.AppRep;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AppServiceImpl implements CrudService<Application> {
+    @Autowired
+    private AppRep appRep;
+
+    @Override
+    public List<Application> getAll() {
+        return appRep.findAll();
+    }
+
+    @Override
+    public Application findById(Long id) {
+        return appRep.findById(id).get();
+    }
+
+    @Override
+    public Application save(Application application) {
+        return appRep.save(application);
+    }
+
+    @Override
+    public Application update(Application application) {
+        return appRep.save(application
+        );
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        appRep.deleteById(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        appRep.deleteAll();
+    }
 }
