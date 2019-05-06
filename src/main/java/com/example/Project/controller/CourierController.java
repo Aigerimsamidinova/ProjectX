458c72220eca6d7b44b8dd2b33a6abb9e06622de
Kanat Subanov <subanovkanat321@gmail.com>
2019-05-06 23:24:29 +0600

    added courier courier controller

--- src/main/java/com/example/Project/controller/CourierController.java
+++ src/main/java/com/example/Project/controller/CourierController.java
@@ -1,4 +1,69 @@
 package com.example.Project.controller;
 
+import com.example.Project.model.Courier;
+import com.example.Project.service.CrudService;
+import com.example.Project.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/office")
 public class CourierController {
+    @Autowired
+    private CrudService<Courier> courierCrudService;
+
+    @GetMapping(path = "/getAll", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<Courier> getAll() {
+        return courierCrudService.getAll();
+    }
+
+    @GetMapping(path = "/get/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Response getById(@PathVariable Long id) {
+        try {
+            return new Response("Courier by id = " + id, true, courierCrudService.findById(id));
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @PostMapping(path = "/add", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Response add(@RequestBody Courier courier) {
+        try {
+            return new Response("Successfully created", true, courierCrudService.save(courier));
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @PutMapping(path = "/update", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    private Response updateOffice(Courier courier) {
+        try {
+            return new Response("Updated", true, courierCrudService.update(courier));
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @DeleteMapping(path = "/delete/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Response deleteById(@PathVariable Long id) {
+        try {
+            courierCrudService.deleteById(id);
+            return new Response("Deleted courier by id = " + id, true, null);
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
+
+    @DeleteMapping(path = "/deleteAll", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    private Response deleteAll() {
+        try {
+            courierCrudService.deleteAll();
+            return new Response("Deleted all couriers", true, null);
+        } catch (Exception e) {
+            return new Response(e.toString(), false, null);
+        }
+    }
 }
